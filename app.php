f4a749a23c0a53d8a0c7cfe6107ecb9e1b8141a6
craig.reeves <craigr@ternio.io>
2018-09-19 00:27:30 -0400

    mod app

--- app.php
+++ app.php
@@ -1,13 +1,13 @@
 <?php
-$http = new swoole_http_server("0.0.0.0", 3001);
+$http = new swoole_http_server("0.0.0.0", 9501);
 
 $http->on("start", function ($server) {
-    echo "Swoole http server is started at http://0.0.0.0:3001\n";
+    echo "Swoole http server is started at http://0.0.0.0:9501\n";
 });
 
 $http->on("request", function ($request, $response) {
     $response->header("Content-Type", "text/plain");
-
+    $response->end("Hello World\n");
 });
 
 $http->start();
