335412574c3218ba23497cbcc89875b251bbd62a
craig.reeves <craigr@ternio.io>
2018-09-18 20:07:47 -0400

    first commit

---
+++ provision.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+sudo apt install -y php7.2-fpm php7.2-mbstring php7.2-xml php7.2-mysql php7.2-fpm php7.2-common php7.2-gd php7.2-json php7.2-cli php7.2-curl php-zmq php-apcu php-memcached \
+php-solr php-mongodb php7.2-pgsql php7.2-opcache php-zmq php-stomp php-imagick php7.2-zip php-pear php7.2-dev php-mongodb
+
+sudo pecl channel-update pecl.php.net
+sudo su -c "yes | pecl install swoole"
+sudo su -c "sudo echo "extension=swoole.so" > /etc/php/7.2/mods-available/swoole.ini"
+ln -s /etc/php/7.2/mods-available/swoole.ini /etc/php/7.2/cli/conf.d/20-swoole.ini
+
